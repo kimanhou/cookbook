fd471269ec5e070f12f46090736aea7ae6c176e7
Kim Anh Nguyen <kimanhou@gmail.com>
2020-04-16 15:22:57 +0200

    Go to home when click on title

--- src/client/AppRoutes.tsx
+++ src/client/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, useParams } from 'react-router-dom';
+import { Route, Switch, useParams, useHistory } from 'react-router-dom';
 import './AppRoutes.scss';
 import RecipeController from './business/controller/RecipeController';
 import LoadData from './view/components/async/LoadData';
@@ -7,10 +7,14 @@ import RecipeListView from './view/recipe/RecipeListView';
 import RecipePage from './view/recipe/RecipePage';
 
 const AppRoutes : React.FC = props => {
+    const history = useHistory();
+    const goToHome = () => {
+        history.push('/');
+    }
 
     return (
         <div className={`app-routes`}>
-          <h1>Cookbook</h1>
+          <h1 onClick={goToHome}>Cookbook</h1>
           <div className={`app-routes-content`}>
                 <Switch>
                     <Route path={`/recipes/:recipeId`}>
